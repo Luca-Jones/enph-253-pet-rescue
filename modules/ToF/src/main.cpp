9f1e7f1345c713c92043dd797ece599c85aa6e1e
luca <lucabuscagliajones@gmail.com>
2025-07-15 10:23:15 -0700

    template code for the ToF test on the pi

---
+++ modules/ToF/src/main.cpp
@@ -0,0 +1,54 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+#define PIN_UART_RX 14
+#define PIN_UART_TX 12
+
+typedef enum tof_reading_e {
+    TOF_READING_NONE    = 0,
+    TOF_READING_PET     = 1,
+    TOF_READING_PILLAR  = 2,
+};
+
+struct tof_data_packet {
+    enum tof_reading_e reading;
+};
+
+void setup() {
+
+    delay(1000);
+
+    /* Computer Serial */
+    Serial.begin(115200);
+
+    /* GPIO Serial */
+    Serial2.begin(115200, SERIAL_8N1, PIN_UART_RX, PIN_UART_TX);
+    Serial.println("UART2 initialized");
+
+}
+
+void loop() {
+
+    struct tof_data_packet tof_data;
+    
+    
+    if (Serial2.available()) {
+
+        Serial2.readBytes((char *) &tof_data, sizeof(tof_data_packet));
+
+        switch (tof_data.reading){
+        case TOF_READING_NONE:
+            Serial.println("No reading...");
+            break;
+        case TOF_READING_PET:
+            Serial.println("Pet detected!");
+            break;
+        case TOF_READING_PILLAR:
+            Serial.println("Pillar detected!");
+            break;
+        default:
+            break;
+        }
+    }
+
+}
---
+++ pi/tof_uart.py
@@ -0,0 +1,20 @@
+import serial
+import time
+from enum import Enum
+
+class tof_reading_e(Enum):
+    TOF_READING_NONE    = 0
+    TOF_READING_PET     = 1
+    TOF_READING_PILLAR  = 2
+
+def ser_setup() -> serial.Serial:
+    ser = serial.Serial(port='/dev/ttyAMA0', baudrate=115200, timeout=1)
+    time.sleep(1);
+
+def send_reading(ser: serial.Serial, reading: tof_reading_e):
+    ser.write(reading)
+
+
+# example
+ser = ser_setup()
+send_reading(ser, reading=tof_reading_e.TOF_READING_NONE)
