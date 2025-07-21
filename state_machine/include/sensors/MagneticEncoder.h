84490736ea4e6fa33e3db3de595c0122de017587
luca <lucabuscagliajones@gmail.com>
2025-07-20 20:50:36 -0700

    implemented the magnetic encoder

--- state_machine/include/sensors/MagneticEncoder.h
+++ state_machine/include/sensors/MagneticEncoder.h
@@ -2,10 +2,19 @@
 #define MAGNETIC_ENCODER_H
 
 #define MAG_I2C_ADDRESS 0x36
+#define MAG_REGISTER_STATUS 0x0B
+#define MAG_MD_BIT 1 << 5
+#define MAG_ML_BIT 1 << 4
+#define MAG_MH_BIT 1 << 3
+#define MAG_REGISTER_RAW_ANGLE_LOW  0x0D
+#define MAG_REGISTER_RAW_ANGLE_HIGH 0x0C
+
+#define MAG_RAW_TO_ANGLE 0.08789f
 
 #define MAG_STATUS_OK           0
 #define MAG_STATUS_TOO_CLOSE    1
 #define MAG_STATUS_TOO_FAR      2
+#define MAG_STATUS_ERROR        3
 
 /**
  * @returns the status of the magnetic encoder
--- state_machine/src/sensors/MagneticEncoder.cpp
+++ state_machine/src/sensors/MagneticEncoder.cpp
@@ -1,15 +1,48 @@
 #include <sensors/MagneticEncoder.h>
-
-// TODO: implement the magnetic encoder
+#include <Wire.h>
 
 int mag_get_status() {
-    return 0;
+
+    Wire.beginTransmission(MAG_I2C_ADDRESS);
+    Wire.write(MAG_REGISTER_STATUS);
+    Wire.endTransmission();
+    Wire.requestFrom(MAG_I2C_ADDRESS, 1); // request 1 byte
+    while (Wire.available() == 0);
+
+    int mag_status = Wire.read();
+    if (mag_status & MAG_MD_BIT) {
+        return MAG_STATUS_OK;
+    } else if (mag_status & MAG_ML_BIT) {
+        return MAG_STATUS_TOO_FAR;
+    } else if (mag_status & MAG_MH_BIT) {
+        return MAG_STATUS_TOO_CLOSE;
+    }
+
+    return MAG_STATUS_ERROR;
 }
 
 void mag_setup() {
-    
+    // any calibration needed goes here
 }
 
 int mag_get_angle() {
-    return 0;
+
+    Wire.beginTransmission(MAG_I2C_ADDRESS);
+    Wire.write(MAG_REGISTER_RAW_ANGLE_LOW);
+    Wire.endTransmission();
+    Wire.requestFrom(MAG_I2C_ADDRESS, 1);  // request 1 byte
+    while (Wire.available() == 0); // wait until there is data to receive
+    
+    int low_byte = Wire.read();
+
+    Wire.beginTransmission(MAG_I2C_ADDRESS);
+    Wire.write(MAG_REGISTER_RAW_ANGLE_HIGH);
+    Wire.endTransmission();
+    Wire.requestFrom(MAG_I2C_ADDRESS, 1);
+    while (Wire.available() == 0);
+
+    int high_byte = Wire.read();
+
+    int raw_angle = (high_byte << 8) | low_byte;
+    return raw_angle * MAG_RAW_TO_ANGLE;
 }
