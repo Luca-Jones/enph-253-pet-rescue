19f8568caa693e7361f2a86a2a8a9bbd3fafdfd8
luca <lucabuscagliajones@gmail.com>
2025-09-01 23:38:36 -0700

    all depricated code moved to depricated/

--- deprecated/cam_demo_web_server/.pio/build/esp32cam/FrameworkArduino/HWCDC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32cam/FrameworkArduino/HWCDC.cpp.o: \
- C:/Users/Luca/.platformio/packages/framework-arduinoespressif32/cores/esp32/HWCDC.cpp \
- C:/Users/Luca/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- C:/Users/Luca/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
